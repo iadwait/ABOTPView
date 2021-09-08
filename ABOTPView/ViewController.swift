65148d8d8bbbe948b6fa9bc7b3714f6fafe82490
Adwait Barkale <adwaitb@winjit.com>
2021-09-08 20:27:58 +0530

    Initial Commit

---
+++ ABOTPView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ABOTPView
+//
+//  Created by Adwait Barkale on 08/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
